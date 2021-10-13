626df2370d6ccadc91fe17966704e2e3711d74d7
kwisho <kwi.shota0912@gmail.com>
2021-10-13 10:21:23 +0900

    <認証を日本語化,ログアウトを実装>

--- backend/resources/views/auth/verify-email.blade.php
+++ backend/resources/views/auth/verify-email.blade.php
@@ -7,12 +7,13 @@
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+            {{ __('登録ありがとうございます！確認の為に登録したメールアドレス宛に
+            届いたメールの確認リンクをクリックして確認してください。') }}
         </div>
 
         @if (session('status') == 'verification-link-sent')
             <div class="mb-4 font-medium text-sm text-green-600">
-                {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                {{ __('登録したメールアドレスに確認リンクが送信されました。') }}
             </div>
         @endif
 
@@ -22,7 +23,7 @@
 
                 <div>
                     <x-button>
-                        {{ __('Resend Verification Email') }}
+                        {{ __('確認用メールを送信') }}
                     </x-button>
                 </div>
             </form>
@@ -31,7 +32,7 @@
                 @csrf
 
                 <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    {{ __('Log Out') }}
+                    {{ __('ログアウト') }}
                 </button>
             </form>
         </div>
--- backend/resources/views/times/index.blade.php
+++ backend/resources/views/times/index.blade.php
@@ -18,8 +18,13 @@
           @endguest
 
           <li class="nav-item">
-            <a class="nav-link active" href="{{ route('logout')}} ">ログアウト</a>
-            @csrf
+            <form method="POST" action="{{ route('logout') }}" >
+              @csrf
+              <a class="nav-link active" href="route('logout')"
+              onclick="event.preventDefault();
+                          this.closest('form').submit();"
+              >ログアウト</a>
+            </form>
           </li>
 
         </ul>
